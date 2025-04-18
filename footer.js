89df53bf9a17baef6e7fb7da8273dae19592047a
Dhrubojit07 <dhrubojitc007@gmail.com>
2025-04-18 16:47:14 +0530

    clode fix

--- footer.js
+++ footer.js
@@ -1,50 +1,44 @@
 const footerHTML = `
   <footer class="section-footer">
-    <div class="footer-container container">
-      <div class="content_1">
-        <img src="./images/logo.png" alt="logo" />
-        <p>
-          Welcome to Thapa EcomStore, <br />
-          your ultimate destination for
-          <br />
-          cutting-edge gadgets!
-        </p>
-        <img src="https://i.postimg.cc/Nj9dgJ98/cards.png" alt="cards" />
-      </div>
-      <div class="content_2">
-        <h4>SHOPPING</h4>
-        <a href="#">Computer Store</a>
-        <a href="#">Laptop Store</a>
-        <a href="#">Accessories</a>
-        <a href="#">Sales & Discount</a>
-      </div>
-      <div class="content_3">
-        <h4>Experience</h4>
-        <a href="./contact.html">Contact Us</a>
-        <a href="" target="_blank"> Payment Method </a>
-        <a href="" target="_blank"> Delivery </a>
-        <a href="" target="_blank"> Return and Exchange </a>
-      </div>
-      <div class="content_4">
-        <h4>NEWSLETTER</h4>
-        <p>
-          Be the first to know about new
-          <br />
-          arrivals, sales & promos!
-        </p>
-        <div class="f-mail">
-          <input type="email" placeholder="Your Email" />
-          <i class="bx bx-envelope"></i>
+      <div class="footer-container container">
+        <div class="content_1">
+          <img src="./images/logo.png" alt="DroneX logo" />
+          <p>
+            Welcome to DroneX, your premier destination for cutting-edge drones<br> 
+            and professional-grade aerial solutions!
+          </p>
+          <img src="https://i.postimg.cc/Nj9dgJ98/cards.png" alt="payment methods" />
         </div>
-        <hr />
+        <div class="content_2">
+          <h4>DRONE SOLUTIONS</h4>
+          <a href="#">Consumer Drones</a>
+          <a href="#">Professional Drones</a>
+          <a href="#">FPV Racing Kits</a>
+          <a href="#">Bundle Offers</a>
+        </div>
+        <div class="content_3">
+          <h4>SUPPORT</h4>
+          <a href="./contact.html">Contact Our Pilots</a>
+          <a href="#">Warranty & Services</a>
+          <a href="#">Shipping Policy</a>
+          <a href="#">FAQ Section</a>
+        </div>
+        <div class="content_4">
+          <h4>FLY WITH US</h4>
+          <p>Get exclusive updates on new drone<br>models and member discounts!</p>
+          <div class="f-mail">
+            <input type="email" placeholder="Enter your pilot email" />
+            <i class="bx bx-envelope"></i>
+          </div>
+          <hr />
+        </div>
       </div>
-    </div>
-    <div class="f-design">
-      <div class="f-design-txt">
-        <p>Design and Code by Thapa Technical</p>
+      <div class="f-design">
+        <div class="f-design-txt">
+          <p>Designed and Developed by Dhrubojit Chowdhury</p>
+        </div>
       </div>
-    </div>
-  </footer>`;
+    </footer>`;
 
 const footerElem = document.querySelector(".section-footer");
 footerElem.insertAdjacentHTML("afterbegin", footerHTML);
