852c5ff268aafdd413ddc8b2af900e317cd5fe02
Dhrubojit07 <158025885+Dhrubojit07@users.noreply.github.com>
2025-04-21 12:36:57 +0530

    image updated

--- getCartProducts.js
+++ getCartProducts.js
@@ -1,14 +1,1 @@
-import { updateCartValue } from "./updateCartValue";
-
-export const getCartProductFromLS = () => {
-  let cartProducts = localStorage.getItem("cartProductLS");
-  if (!cartProducts) {
-    return [];
-  }
-  cartProducts = JSON.parse(cartProducts);
-
-  //update the cart button value
-  updateCartValue(cartProducts);
-
-  return cartProducts;
-};
+import{updateCartValue}from"./updateCartValue";export const getCartProductFromLS=()=>{let t=localStorage.getItem("cartProductLS");return t?(t=JSON.parse(t),updateCartValue(t),t):[]};
